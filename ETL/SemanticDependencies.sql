f7bc8359b2b6f768f4de9de6aa89582fee2fd3eb
Ludovik TEKAM <11706765@f207-0.ig-edu.univ-paris13.fr>
2020-01-18 14:35:49 +0100

    [DR_SemanticDependencies OK][organizer code]

--- ETL/SemanticDependencies.sql
+++ ETL/SemanticDependencies.sql
@@ -12,6 +12,7 @@ LEFTCOL VARCHAR2(500),
 SEMANTICDEPENDENCY VARCHAR2(4),
 RIGHTCOL VARCHAR2(100),
 PERCENTAGE NUMBER,          --Pourcentage de la vérification de la relation sémantique
+CONSTRAINT PK_SEMDEP PRIMARY KEY (TableName,LEFTCOL,SEMANTICDEPENDENCY,RIGHTCOL,PERCENTAGE),
 CONSTRAINT CK_SEMDEP CHECK (SEMANTICDEPENDENCY IN ('-DF-', '-EQ-', '-LE-', '-GE-'))
 );
 
@@ -118,57 +119,89 @@ END;
 
 
 CREATE OR REPLACE PROCEDURE VERIFDF_TAB(tableName IN VARCHAR2,
- DR_SemanticDependencies IN VARCHAR2)AS
+ DR_SemanticDependencies IN VARCHAR2)
+ AS
 
-table_cursor SYS_REFCURSOR;
-table_cursor2 SYS_REFCURSOR;
-myQuery VARCHAR2(500);
+  table_cursor SYS_REFCURSOR;
+  table_cursor2 SYS_REFCURSOR;
+  myQuery VARCHAR2(500);
 
-selected_values VARCHAR2(500);
-where_conditions VARCHAR2(500);
+  selected_values VARCHAR2(500);
+  where_conditions VARCHAR2(500);
 
-LEFTCOL VARCHAR2(500);
-RIGHTCOL VARCHAR2(500);
-pct NUMBER;
-myQuery_RIGHTCOL VARCHAR2(500);
-myQuery_LEFTCOL VARCHAR2(500);
+  LEFTCOL VARCHAR2(500);
+  RIGHTCOL VARCHAR2(500);
 
-nbrCol  NUMBER;
-maxCol  NUMBER;
+  tableName_name VARCHAR2(500);
+  LEFTCOL_name VARCHAR2(500);
+  RIGHTCOL_name VARCHAR2(500);
+  SEMANTICDEPENDENCY_name VARCHAR2(500);
+  PERCENTAGE_name VARCHAR2(500);
+
+  pct NUMBER;
+  myQuery_RIGHTCOL VARCHAR2(500);
+  myQuery_LEFTCOL VARCHAR2(500);
+
+  nbrCol  NUMBER;
+  maxCol  NUMBER;
 BEGIN
---- cette requête permet de récupérer les colonnes qu'une table passer en paramètres
-selected_values := ' COLUMN_NAME ';
-where_conditions := ' where table_name = '''||upper(tableName)||''' ';
---where_conditions := ' where table_name = '''||upper(tableName)||''' AND DATA_TYPE = ''VARCHAR2''';
-myQuery_LEFTCOL := 'SELECT '|| selected_values ||' FROM user_tab_columns'|| where_conditions;
-myQuery_RIGHTCOL := myQuery_LEFTCOL;
+  print_debug (' +---------------------------------------------------------------------+ ');
+  print_debug (' [ DETECT SEMANTIC DEPENDENCIES ] ');
+  --- cette requête permet de récupérer les colonnes qu'une table passer en paramètres
+  selected_values := ' COLUMN_NAME ';
+  where_conditions := ' where table_name = '''||upper(tableName)||''' ';
+  --where_conditions := ' where table_name = '''||upper(tableName)||''' AND DATA_TYPE = ''VARCHAR2''';
+  myQuery_LEFTCOL := 'SELECT '|| selected_values ||' FROM user_tab_columns'|| where_conditions;
+  myQuery_RIGHTCOL := myQuery_LEFTCOL;
 
-myQuery := 'SELECT COUNT(*) FROM user_tab_columns'|| where_conditions;
-EXECUTE IMMEDIATE myQuery INTO maxCol;
---- pour chaque colonne récupérée, je fait la verification des DF
-nbrCol := 0;
-open table_cursor for myQuery_LEFTCOL;
-  loop
-    fetch table_cursor into LEFTCOL ;
-    EXIT when table_cursor%NOTFOUND OR nbrCol = maxCol - 1 ;
-    -------------------------------------------
-    open table_cursor2 for myQuery_RIGHTCOL;
-      loop
-        fetch table_cursor2 into RIGHTCOL ;
-        EXIT when table_cursor2%NOTFOUND;
-        --print_debug(RIGHTCOL);
-        IF LEFTCOL NOT LIKE RIGHTCOL THEN
+  myQuery := 'SELECT COUNT(*) FROM user_tab_columns'|| where_conditions;
+  EXECUTE IMMEDIATE myQuery INTO maxCol;
+  --- pour chaque colonne récupérée, je fait la verification des DF
+  nbrCol := 0;
+  open table_cursor for myQuery_LEFTCOL;
+    loop
+      fetch table_cursor into LEFTCOL ;
+      EXIT when table_cursor%NOTFOUND OR nbrCol = maxCol - 1 ;
+      -------------------------------------------
+      open table_cursor2 for myQuery_RIGHTCOL;
+        loop
+          fetch table_cursor2 into RIGHTCOL ;
+          EXIT when table_cursor2%NOTFOUND;
+          --print_debug(RIGHTCOL);
+          IF LEFTCOL NOT LIKE RIGHTCOL THEN
 
-          --pct := VERIFDF(tableName,DR_SemanticDependencies,LEFTCOL,'COL2_NAMES');
-          pct := VERIFDF(tableName,DR_SemanticDependencies,LEFTCOL,RIGHTCOL);
-          print_debug(LEFTCOL || '-------' || RIGHTCOL || '--' || pct);
-        END IF;
-      end loop;
-    close table_cursor2;
-    --------------------------------------------
-    nbrCol := nbrCol +1;
-  end loop;
-close table_cursor;
+            --pct := VERIFDF(tableName,DR_SemanticDependencies,LEFTCOL,'COL2_NAMES');
+            pct := VERIFDF(tableName,DR_SemanticDependencies,LEFTCOL,RIGHTCOL);
+            print_debug (' ----- ['|| LEFTCOL ||' ('||ROUND(pct)||'%) '||RIGHTCOL||' ]');
+
+            --print_debug(LEFTCOL || '-------' || RIGHTCOL || '--' || pct);
+          END IF;
+        end loop;
+      close table_cursor2;
+      --------------------------------------------
+      nbrCol := nbrCol +1;
+    end loop;
+  close table_cursor;
+  ----------------------------------------------------------------------
+  print_debug (' ------- +++ [ COMBINING LEFTCOL ] ');
+  tableName_name := 'tableName';
+  LEFTCOL_name := 'LEFTCOL';
+  RIGHTCOL_name := 'RIGHTCOL';
+  SEMANTICDEPENDENCY_name := 'SEMANTICDEPENDENCY';
+  PERCENTAGE_name := 'PERCENTAGE';
+  
+
+  myQuery := 'INSERT INTO '||DR_SemanticDependencies||' SELECT '||tableName_name
+  ||',LISTAGG('||LEFTCOL_name||', '' + '') WITHIN GROUP (ORDER BY '||LEFTCOL_name||'), '||SEMANTICDEPENDENCY_name
+  ||','||RIGHTCOL_name||',ROUND(AVG('||PERCENTAGE_name||')) FROM   '||DR_SemanticDependencies
+  ||' WHERE '||SEMANTICDEPENDENCY_name||' = ''-DF-'' GROUP BY '||
+  tableName_name||', '|| RIGHTCOL_name||','|| SEMANTICDEPENDENCY_name;
+  print_debug (' ----- [  ');
+  print_debug (' -------  '|| myQuery ||' ] ');
+  print_debug (' ----- ]  ');
+  EXECUTE IMMEDIATE myQuery ;
+  print_debug (' ---------- [ select * from '|| DR_SemanticDependencies ||'; ] ');
+  print_debug (' +---------------------------------------------------------------------+ ');
 END;
 /
 
@@ -193,6 +226,7 @@ BEGIN
 VERIFDF_TAB('CSVfile1_new','DR_SemanticDependencies');
 
 
+
 END;
 /
 
@@ -200,10 +234,26 @@ END;
 
 --select * from DR_SemanticDependencies ORDER BY PERCENTAGE;
 
-select count(*),RIGHTCOL from DR_SemanticDependencies GROUP BY RIGHTCOL ORDER BY RIGHTCOL;
+--select count(*),RIGHTCOL from DR_SemanticDependencies GROUP BY RIGHTCOL ORDER BY RIGHTCOL;
 
-select * from DR_SemanticDependencies ORDER BY RIGHTCOL;
-SELECT RIGHTCOL,ROUND(AVG(PERCENTAGE)), LISTAGG(LEFTCOL, ' + ') WITHIN GROUP (ORDER BY LEFTCOL) AS new_leftcol
-FROM   DR_SemanticDependencies WHERE SEMANTICDEPENDENCY = '-DF-' GROUP BY RIGHTCOL;
+COLUMN TableName_new             FORMAT A15
+COLUMN LEFTCOL_new               FORMAT A40
+COLUMN SEMANTICDEPENDENCY_new    FORMAT A30
+COLUMN RIGHTCOL_new              FORMAT A25
+COLUMN PERCENTAGE_new            FORMAT 999
+
+--select * from DR_SemanticDependencies ORDER BY RIGHTCOL;
+-- SELECT tableName,
+-- LISTAGG(LEFTCOL, ' + ') WITHIN GROUP (ORDER BY LEFTCOL) AS LEFTCOL_new,
+-- SEMANTICDEPENDENCY,
+-- RIGHTCOL,
+-- ROUND(AVG(PERCENTAGE)) as PERCENTAGE_new
+-- FROM   DR_SemanticDependencies 
+-- WHERE SEMANTICDEPENDENCY = '-DF-' GROUP BY tableName,RIGHTCOL,SEMANTICDEPENDENCY;
 
 --@SemanticDependencies.sql
+--M2EID_23/M2EID_23
+
+--INSERT INTO DR_SemanticDependencies 
+--SELECT tableName,LISTAGG(LEFTCOL, ' + ') WITHIN GROUP (ORDER BY LEFTCOL), SEMANTICDEPENDENCY,RIGHTCOL,ROUND(AVG(PERCENTAGE)) FROM   DR_SemanticDependencies WHERE SEMANTICDEPENDENCY = '-DF-' GROUP BY tableName,RIGHTCOL,SEMANTICDEPENDENCY;
+--select * from DR_SemanticDependencies ORDER BY RIGHTCOL;
